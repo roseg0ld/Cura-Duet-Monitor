348bf5454bb8740f4a9ff43a554b458a83d5131f
Zach <zachrose@hydraresearch3d.com>
2019-06-07 17:14:52 -0700

    initial commit

---
+++ request.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def statusRetrieval(url, cat1, cat2, index):
+    return json.loads(requests.get('http://' + url + '/rr_status?type=1').text)[cat1][cat2][index]
+
+def sendGcode(url,gcode):
+    return requests.get('http://' + url + '/rr_gcode?gcode='+gcode)
+
+url = '10.1.10.3'
+coordx = statusRetrieval(url, 'coords','xyz', 0)
+coordy = statusRetrieval(url, 'coords','xyz', 1)
+coordz = statusRetrieval(url, 'coords','xyz', 2)
+tempbed = statusRetrieval(url, 'temps','current', 0)
+tempext = statusRetrieval(url, 'temps','current', 1)
+
+
+print('The Toolhead is at: (',coordx,', ',coordy,', ',coordz,')')
+print('The Extruder is ',tempext,'°C')
+print('The Bed is ',tempbed,'°C')
+sendGcode(url,'M104 S35')
+
+#print(type(response))
